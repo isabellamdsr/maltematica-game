dc8585c9b2845871c4e9c00ba2c47ae8ab01ae1d
Isabella Mendes <194180570+isabellamdsr@users.noreply.github.com>
2025-03-27 16:21:26 -0300

    Add files via upload
    modulo do projetil criado

---
+++ moduloProjetil.py
@@ -0,0 +1,15 @@
+import pygame
+import random
+
+#projetil settings
+projetil_size = 20
+projetil_speed = 12
+
+class Projetil:
+    def __init__(self, naturaisLista, WIDTH):
+        self.image = pygame.image.load(naturaisLista[random.randint(0,9)])  # Carregar imagem do jogador
+        self.image = pygame.transform.scale(self.image, (projetil_size, projetil_size))  # Ajustar o tamanho da imagem
+        self.rect = self.image.get_rect(center=(random.randint(0, WIDTH - projetil_size), 0))  # Usar o retângulo da imagem
+    
+    def move(self):
+        self.rect.y += projetil_speed #Projetil cai pro inferno
