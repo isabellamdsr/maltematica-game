eb7618a0620d3855895563b89a23a974256bb40d
Isabella Mendes <194180570+isabellamdsr@users.noreply.github.com>
2025-03-27 17:29:12 -0300

    Add files via upload
    modulo das configuracoes criado

---
+++ moduloConfig.py
@@ -0,0 +1,40 @@
+import pygame
+
+#Tela 
+WIDTH, HEIGHT = 1440, 810
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Top-Down Shooter")
+
+#Cores
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+#Sprites
+   # Naturais
+naturaisLista=['spritesGT/Naturais/zero.png','spritesGT/Naturais/um.webp','spritesGT/Naturais/dois.png',
+               'spritesGT/Naturais/tres.png','spritesGT/Naturais/quatro.png','spritesGT/Naturais/cinco.png',
+               'spritesGT/Naturais/seis.png','spritesGT/Naturais/sete.png','spritesGT/Naturais/oito.png',
+               'spritesGT/Naturais/nove.png']
+
+   # Animação Navin
+navinLista=['spritesGT/navin/navin_idle.png','spritesGT/navin/navin_idle2.png','spritesGT/navin/navin_idle3.png','spritesGT/navin/navin_idle4.png']
+
+#Configs Gerais
+
+#player config
+player_size = 66
+player_size2 = 60
+player_speed = 10
+
+#bullet config
+bullet_size = 21
+bullet_speed = 20
+
+#enemy config
+enemy_size = 50
+enemy_speed = 2
+
+#projetil config
+projetil_size = 20
+projetil_speed = 12
